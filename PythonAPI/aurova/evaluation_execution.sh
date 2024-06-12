8521b287a89c8c53459582dc4b4906242a5ee8a2
Alejandro Olivas González <alejandro.olivas@ua.es>
2024-06-12 15:17:42 +0200

    Full evaluation or evaluation without complex maneuvers

--- PythonAPI/aurova/evaluation_execution.sh
+++ PythonAPI/aurova/evaluation_execution.sh
@@ -17,13 +17,14 @@ function handle_interrupt {
     exit 1
 }
 
-start_test=0
+full_evaluation=false
+start_test=3
 wait_launch=14
 wait=5
-output=MTAP
-architecture=aurova
+output=transfuser
+architecture=transfuser
 pedestrian_routes_prefix=routes/pedestrian_routes/
-export CARLA_RESULTS_PATH="/media/alolivas/MSI_500/carla_limpio/carla/PythonAPI/aurova/results_prueba/"
+export CARLA_RESULTS_PATH="/media/alolivas/MSI_500/carla_limpio/carla/PythonAPI/aurova/results_simple/"
 
 #Town01
 export CARLA_MAP="/media/alolivas/MSI_500/aurova_carla/carla/PythonAPI/aurova/routes/Town01.xml"
@@ -31,10 +32,18 @@ export OSM_ROS="/home/alolivas/aurova-lab/aurova_ws/src/applications/app_old/pat
 export LAT_ZERO="38.38164422311365"
 export LON_ZERO="-0.5255029920793286"
 
-way_id=(6 8 9 10 11 12 14)
-pedestrian_routes=(Town01_evaluation1.xml Town01_evaluation2.xml Town01_evaluation3.xml Town01_evaluation4.xml Town01_evaluation4.xml Town01_evaluation4.xml  Town01_evaluation5.xml)
-close_loop=("0 1" "0 3" "3 0" "0 5" "5 4" "4 5" "6 7")
-weather=(6 2 0 6 1 1 4)
+
+if [ "$full_evaluation" = true ]; then
+  way_id=(6 8 9 10 11 12 14)
+  pedestrian_routes=(Town01_evaluation1_full.xml Town01_evaluation2_full.xml Town01_evaluation3_full.xml Town01_evaluation4_full.xml Town01_evaluation4_full.xml Town01_evaluation4_full.xml  Town01_evaluation5_full.xml)
+  close_loop=("0 1" "0 3" "3 0" "0 5" "5 4" "4 5" "6 7")
+  weather=(6 2 0 6 1 1 4)
+else
+  way_id=(6 8 10 11 12 14)
+  pedestrian_routes=(Town01_evaluation1.xml Town01_evaluation2.xml Town01_evaluation4.xml Town01_evaluation4.xml Town01_evaluation4.xml  Town01_evaluation5.xml)
+  close_loop=("0 1" "0 3" "0 5" "5 4" "4 5" "6 7")
+  weather=(6 2 6 1 1 4)
+fi
 
 # Town02
 # export CARLA_MAP="/media/alolivas/MSI_500/aurova_carla/carla/PythonAPI/aurova/routes/Town02.xml"
@@ -42,10 +51,17 @@ weather=(6 2 0 6 1 1 4)
 # export LAT_ZERO="38.3799",
 # export LON_ZERO="0.0",
 
-# way_id=(1 2 3 4 5 6 7 8 9 10 11)
-# pedestrian_routes=(Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation2.xml Town02_evaluation2.xml Town02_evaluation2.xml)
-# close_loop=("1 2" "3" "1" "4 3" "6 5" "1 0" "4" "2" "3 4" "1 2" "5 6")
-# weather=(6 1 1 2 2 6 2 6 1 0 6)
+# if [ "$full_evaluation" = true ]; then
+#   way_id=(1 2 3 4 5 6 7 8 9 10 11)
+#   pedestrian_routes=(Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation1_full.xml Town02_evaluation2_full.xml Town02_evaluation2_full.xml Town02_evaluation2_full.xml)
+#   close_loop=("1 2" "3" "1" "4 3" "6 5" "1 0" "4" "2" "3 4" "1 2" "5 6")
+#   weather=(6 1 1 2 2 6 2 6 1 0 6)
+# else
+#   way_id=(1 2 4 5 6 7 8 9 11)
+#   pedestrian_routes=(Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation1.xml Town02_evaluation2.xml Town02_evaluation2.xml)
+#   close_loop=("1 2" "3" "4 3" "6 5" "1 0" "4" "2" "3 4" "5 6")
+#   weather=(6 1 2 2 6 2 6 1 6)
+# fi
 
 #Town03
 # export CARLA_MAP="/media/alolivas/MSI_500/aurova_carla/carla/PythonAPI/aurova/routes/Town03.xml"
@@ -53,6 +69,11 @@ weather=(6 2 0 6 1 1 4)
 # export LAT_ZERO="38.3811"
 # export LON_ZERO="0.001"
 
+# if [ "$full_evaluation" = true ]; then
+#   pedestrian_routes=(Town03_evaluation1_full.xml)
+# else
+#   pedestrian_routes=(Town03_evaluation1.xml)
+# fi
 # way_id=(-1)
 # pedestrian_routes=(Town03_evaluation1.xml)
 # closed_loop=("5 4 3 2 1 0")
@@ -64,10 +85,17 @@ weather=(6 2 0 6 1 1 4)
 # export LAT_ZERO="38.3839"
 # export LON_ZERO="0.001"
 
-# way_id=(2 3 4 5)
-# pedestrian_routes=(Town04_evaluation1.xml Town04_evaluation1.xml Town04_evaluation1.xml Town04_evaluation1.xml)
-# close_loop=("11 7" "2 3 4 5 6 7" "0 7" "9 0")
-# weather=(6 2 0 4)
+# if [ "$full_evaluation" = true ]; then
+#   way_id=(2 3 4 5)
+#   pedestrian_routes=(Town04_evaluation1_full.xml Town04_evaluation1_full.xml Town04_evaluation1_full.xml Town04_evaluation1_full.xml)
+#   close_loop=("11 7" "2 3 4 5 6 7" "0 7" "9 0")
+#   weather=(6 2 0 4)
+# else
+#   way_id=(2 3 5)
+#   pedestrian_routes=(Town04_evaluation1.xml Town04_evaluation1.xml Town04_evaluation1.xml)
+#   close_loop=("11 7" "2 3 4 5 6 7" "9 0")
+#   weather=(6 2 4)
+# fi
 
 #Town05
 # export CARLA_MAP="/media/alolivas/MSI_500/aurova_carla/carla/PythonAPI/aurova/routes/Town05.xml"
@@ -75,10 +103,17 @@ weather=(6 2 0 6 1 1 4)
 # export LAT_ZERO="38.381"
 # export LON_ZERO="0.0"
 
-# way_id=(2 3 4 5 6 7 8)
-# pedestrian_routes=(Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml)
-# close_loop=("6" "7" "7" "9 10 11" "11" "12 0" "1 2 3 4 5")
-# weather=(6 1 1 6 6 2 2)
+# if [ "$full_evaluation" = true ]; then
+#   way_id=(2 3 4 5 6 7 8)
+#   pedestrian_routes=(Town05_evaluation1_full.xml Town05_evaluation1_full.xml Town05_evaluation1_full.xml Town05_evaluation1_full.xml Town05_evaluation1_full.xml Town05_evaluation1_full.xml Town05_evaluation1_full.xml)
+#   close_loop=("6" "7" "7" "9 10 11" "11" "12 0" "1 2 3 4 5")
+#   weather=(6 1 1 6 6 2 2)
+# else
+#   way_id=(2 3 5 6 8)
+#   pedestrian_routes=(Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml Town05_evaluation1.xml)
+#   close_loop=("6" "7" "9 10 11" "11" "1 2 3 4 5")
+#   weather=(6 1 6 6 2)
+# fi
 
 
 for ((i=$start_test; i<${#way_id[@]}; i++))
